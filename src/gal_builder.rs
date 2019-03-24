1b038385eac17343c1d6d2cb7374001817ec2627
Simon Frankau <sgf@arbitrary.name>
2019-03-24 15:28:21 +0000

    Tidy set_and

--- src/gal_builder.rs
+++ src/gal_builder.rs
@@ -18,41 +18,41 @@ const ROW_COUNT_20RA10: usize = 80;
 
 // GAL16V8
 
-const PinToFuse16Mode1: [i32; 20] = [
+const PIN_TO_FUSE_16_MODE1: [i32; 20] = [
     2, 0, 4, 8, 12, 16, 20, 24, 28, -1, 30, 26, 22, 18, -1, -1, 14, 10, 6, -1,
 ];
 
-const PinToFuse16Mode2: [i32; 20] = [
+const PIN_TO_FUSE_16_MODE2: [i32; 20] = [
     2, 0, 4, 8, 12, 16, 20, 24, 28, -1, 30, -1, 26, 22, 18, 14, 10, 6, -1, -1,
 ];
 
-const PinToFuse16Mode3: [i32; 20] = [
+const PIN_TO_FUSE_16_MODE3: [i32; 20] = [
     -1, 0, 4, 8, 12, 16, 20, 24, 28, -1, -1, 30, 26, 22, 18, 14, 10, 6, 2, -1,
 ];
 
 // GAL20V8
 
-const PinToFuse20Mode1: [i32; 24] = [
+const PIN_TO_FUSE_20_MODE1: [i32; 24] = [
     2, 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, -1, 38, 34, 30, 26, 22, -1, -1, 18, 14, 10, 6, -1,
 ];
 
-const PinToFuse20Mode2: [i32; 24] = [
+const PIN_TO_FUSE_20_MODE2: [i32; 24] = [
     2, 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, -1, 38, 34, -1, 30, 26, 22, 18, 14, 10, -1, 6, -1,
 ];
 
-const PinToFuse20Mode3: [i32; 24] = [
+const PIN_TO_FUSE_20_MODE3: [i32; 24] = [
     -1, 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, -1, -1, 38, 34, 30, 26, 22, 18, 14, 10, 6, 2, -1,
 ];
 
 // GAL22V10
 
-const PinToFuse22V10: [i32; 24] = [
+const PIN_TO_FUSE_22V10: [i32; 24] = [
     0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, -1, 42, 38, 34, 30, 26, 22, 18, 14, 10, 6, 2, -1,
 ];
 
 // GAL20RA10
 
-const PinToFuse20RA10: [i32; 24] = [
+const PIN_TO_FUSE_20RA10: [i32; 24] = [
     -1, 0, 4, 8, 12, 16, 20, 24, 28, 32, 36, -1, -1, 38, 34, 30, 26, 22, 18, 14, 10, 6, 2, -1,
 ];
 
@@ -107,23 +107,25 @@ fn set_and(
         _ => panic!("Nope"),
     };
 
-    let column = match gal_type {
+    let column_lookup: &[i32] = match gal_type {
         GAL16V8 => match mode {
-            MODE1 => PinToFuse16Mode1[pin_num - 1],
-            MODE2 => PinToFuse16Mode2[pin_num - 1],
-            MODE3 => PinToFuse16Mode3[pin_num - 1],
+            MODE1 => &PIN_TO_FUSE_16_MODE1,
+            MODE2 => &PIN_TO_FUSE_16_MODE2,
+            MODE3 => &PIN_TO_FUSE_16_MODE3,
             _ => panic!("Nope"),
         },
         GAL20V8 => match mode {
-            MODE1 => PinToFuse20Mode1[pin_num - 1],
-            MODE2 => PinToFuse20Mode2[pin_num - 1],
-            MODE3 => PinToFuse20Mode3[pin_num - 1],
+            MODE1 => &PIN_TO_FUSE_20_MODE1,
+            MODE2 => &PIN_TO_FUSE_20_MODE2,
+            MODE3 => &PIN_TO_FUSE_20_MODE3,
             _ => panic!("Nope"),
         },
-        GAL22V10 => PinToFuse22V10[pin_num - 1],
-        GAL20RA10 => PinToFuse20RA10[pin_num - 1],
+        GAL22V10 => &PIN_TO_FUSE_22V10,
+        GAL20RA10 => &PIN_TO_FUSE_20RA10,
         _ => panic!("Nope"),
-    } as usize;
+    };
+
+    let column = column_lookup[pin_num - 1] as usize;
 
     // Is it a registered OLMC pin?
     // If yes, then correct the negation.
