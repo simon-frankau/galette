f31e3e9dab2540dfc110327ed046ec489d408b40
Simon Frankau <sgf@arbitrary.name>
2019-03-24 19:36:43 +0000

    Add support for fuse files

--- src/writer.rs
+++ src/writer.rs
@@ -132,8 +132,6 @@ pub extern "C" fn write_pin_c(
     }
 }
 
-
-
 fn make_pin(gal_type: i32, pin_names: &[&str], mode: i32, olmc_pin_types: &[i32]) -> String {
     let num_of_pins = pin_names.len();
 
@@ -204,5 +202,167 @@ fn make_pin(gal_type: i32, pin_names: &[&str], mode: i32, olmc_pin_types: &[i32]
         }
     }
 
+    return buf;
+}
+
+fn make_row(buf: &mut String, num_of_col: usize, row: usize, data: &[u8]) {
+    buf.push_str(&format!("\n{:>3} ", row));
+
+    for col in 0..num_of_col {
+        if col % 4 == 0 {
+            buf.push_str(" ");
+        }
+
+        if data[row * num_of_col + col] != 0 {
+            buf.push_str("-");
+        } else {
+            buf.push_str("x");
+        }
+    }
+}
+
+const OLMC_SIZE_22V10: [i32; 12] = [ 9, 11, 13, 15, 17, 17, 15, 13, 11, 9, 1, 1 ];
+
+fn get_size(gal_type: i32, olmc: usize) -> i32
+{
+    match gal_type {
+    GAL16V8  => 8,
+    GAL20V8  => 8,
+    GAL22V10 => OLMC_SIZE_22V10[olmc],
+    GAL20RA10 => 8,
+    _ => panic!("Nope")
+    }
+}
+
+// Number of fuses per-row.
+const ROW_LEN_ADR16: usize = 32;
+const ROW_LEN_ADR20: usize = 40;
+const ROW_LEN_ADR22V10: usize = 44;
+const ROW_LEN_ADR20RA10: usize = 40;
+
+// Number of rows of fuses.
+const ROW_COUNT_16V8: usize = 64;
+const ROW_COUNT_20V8: usize = 64;
+const ROW_COUNT_22V10: usize = 132;
+const ROW_COUNT_20RA10: usize = 80;
+
+const AC1_SIZE: usize = 8;
+
+#[no_mangle]
+pub extern "C" fn write_fuse_c(
+    file_name: *const c_char,
+    gal_type: i32,
+    pin_names: *const * const c_char,
+    gal_fuses: *const u8,
+    gal_xor: *const u8,
+    gal_ac1: *const u8,
+    gal_s1: *const u8,
+) {
+    let xor_size = match gal_type {
+        GAL16V8 => 8,
+        GAL20V8 => 8,
+        GAL22V10 => 10,
+        GAL20RA10 => 10,
+        _ => panic!("Nope"),
+    };
+
+    let fuse_size = match gal_type {
+        GAL16V8 => ROW_LEN_ADR16 * ROW_COUNT_16V8,
+        GAL20V8 => ROW_LEN_ADR20 * ROW_COUNT_20V8,
+        GAL22V10 => ROW_LEN_ADR22V10 * ROW_COUNT_22V10,
+        GAL20RA10 => ROW_LEN_ADR20RA10 * ROW_COUNT_20RA10,
+        _ => panic!("Nope"),
+    };
+
+    unsafe {
+        let file_name_rs = CStr::from_ptr(file_name);
+
+        let num_pins = if gal_type == GAL16V8 { 20 } else { 24 };
+
+        let cstrs = std::slice::from_raw_parts(pin_names, num_pins);
+        let pin_names = cstrs.iter().map(|x| CStr::from_ptr(*x).to_str().unwrap()).collect::<Vec<_>>();
+
+        let str = make_fuse(gal_type, &pin_names,
+            std::slice::from_raw_parts(gal_fuses, fuse_size),
+            std::slice::from_raw_parts(gal_xor, xor_size),
+            std::slice::from_raw_parts(gal_ac1, AC1_SIZE),
+            std::slice::from_raw_parts(gal_s1, 10));
+
+        let mut file = File::create(file_name_rs.to_str().unwrap()).unwrap();
+        file.write_all(str.as_bytes());
+    }
+}
+
+fn make_fuse(gal_type: i32, pin_names: &[&str], gal_fuse: &[u8], gal_xor: &[u8], gal_ac1: &[u8], gal_s1: &[u8]) -> String {
+    let mut buf = String::new();
+
+    let (mut pin, num_olmcs) = match gal_type {
+        GAL16V8   => (19, 8),
+        GAL20V8   => (22, 8),
+        GAL22V10  => (23, 10),
+        GAL20RA10 => (23, 10),
+        _ => panic!("Nope"),
+    };
+
+    let row_len = match gal_type {
+        GAL16V8   => ROW_LEN_ADR16,
+        GAL20V8   => ROW_LEN_ADR20,
+        GAL22V10  => ROW_LEN_ADR22V10,
+        GAL20RA10 => ROW_LEN_ADR20RA10,
+        _ => panic!("Nope"),
+    };
+
+    let mut row = 0;
+
+    for olmc in 0..num_olmcs {
+        if gal_type == GAL22V10 && olmc == 0 {
+            // AR when 22V10
+            buf.push_str("\n\nAR");
+            make_row(&mut buf, row_len, row, gal_fuse);
+            row += 1;
+        }
+
+        let num_rows = get_size(gal_type, olmc);
+
+        // Print pin
+        buf.push_str(&format!("\n\nPin {:>2} = ", pin));
+
+        buf.push_str(&format!("{}", pin_names[pin - 1]));
+
+        make_spaces(&mut buf, 13 - pin_names[pin - 1].len());
+
+        match gal_type {
+            GAL16V8 => {
+                buf.push_str(&format!("XOR = {:>1}   AC1 = {:>1}", gal_xor[19 - pin], gal_ac1[19 - pin]));
+            }
+            GAL20V8 => {
+                buf.push_str(&format!("XOR = {:>1}   AC1 = {:>1}", gal_xor[22 - pin], gal_ac1[22 - pin]));
+            }
+            GAL22V10 => {
+                buf.push_str(&format!("S0 = {:>1}   S1 = {:>1}", gal_xor[23 - pin], gal_s1[23 - pin]));
+            }
+            GAL20RA10 => {
+                buf.push_str(&format!("S0 = {:>1}", gal_xor[23 - pin]));
+            }
+            _ => panic!("Nope"),
+        };
+
+        for n in 0..num_rows {
+            // Print all fuses of an OLMC
+            make_row(&mut buf, row_len, row, gal_fuse);
+            row += 1;
+        }
+
+
+        if gal_type == GAL22V10 && olmc == 9 {
+            // SP when 22V10
+            buf.push_str("\n\nSP");
+            make_row(&mut buf, row_len, row, gal_fuse);
+        }
+
+        pin -= 1;
+    }
+
+    buf.push_str("\n\n");
     return buf;
 }
