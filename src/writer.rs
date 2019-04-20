956a1c6ee4dab4cf795e507443cc4db6c88c65b9
Simon Frankau <sgf@arbitrary.name>
2019-04-21 00:28:53 +0100

    Tidy make_chip and make_pin

--- src/writer.rs
+++ src/writer.rs
@@ -7,13 +7,6 @@ use std::io::Error;
 use std::io::Write;
 use std::path::PathBuf;
 
-
-fn make_spaces(buf: &mut String, n: usize) {
-    for _i in 0..n {
-        buf.push(' ');
-    }
-}
-
 ////////////////////////////////////////////////////////////////////////
 // 'make_chip' draws out the chip with pin assignments.
 //
@@ -22,30 +15,19 @@ fn make_chip(gal_type: Chip, pin_names: &[String]) -> String {
     let num_of_pins = pin_names.len();
     let mut buf = String::new();
 
-    buf.push_str("\n\n");
-
-    make_spaces(&mut buf, 31);
-
-    // TODO: Shuffle name a space left for the RA10 for alignment.
-    buf.push_str(&format!(" {}\n\n", gal_type.name()));
-
-    make_spaces(&mut buf, 26);
-
-    buf.push_str("-------\\___/-------\n");
+    buf.push_str(format!("\n\n{:^72}", gal_type.name()).trim_right());
+    buf.push_str(&format!("\n\n{:25} -------\\___/-------", ""));
 
     let mut started = false;
     for n in 0..num_of_pins / 2 {
         if started {
-            make_spaces(&mut buf, 26);
-            buf.push_str("|                 |\n");
+            buf.push_str(&format!("\n{:25} |                 |", ""));
         } else {
             started = true;
         }
 
-        make_spaces(&mut buf, 25 - pin_names[n].len());
-
         buf.push_str(&format!(
-            "{} | {:>2}           {:>2} | {}\n",
+            "\n{:>25} | {:>2}           {:>2} | {}",
             pin_names[n],
             n + 1,
             num_of_pins - n,
@@ -53,8 +35,7 @@ fn make_chip(gal_type: Chip, pin_names: &[String]) -> String {
         ));
     }
 
-    make_spaces(&mut buf, 26);
-    buf.push_str("-------------------\n");
+    buf.push_str(&format!("\n{:25} -------------------\n", ""));
 
     return buf;
 }
@@ -63,74 +44,44 @@ fn make_chip(gal_type: Chip, pin_names: &[String]) -> String {
 // 'make_pin' lists the pin assignments.
 //
 
-fn make_pin(gal: &GAL, pin_names: &[String], olmcs: &[OLMC]) -> String {
-    let num_of_pins = pin_names.len();
-    let gal_type = gal.chip;
+fn pin_type(gal: &GAL, olmcs: &[OLMC], i: usize) -> &'static str {
+    let chip = gal.chip;
+    let num_pins = chip.num_pins();
 
+    if let Some(olmc) = chip.pin_to_olmc(i) {
+        let olmc = &olmcs[olmc];
+        if olmc.output.is_some() {
+            "Output"
+        } else if !olmc.feedback {
+            "NC"
+        } else {
+            "Input"
+        }
+    } else if i == num_pins / 2 {
+        "GND"
+    } else if i == num_pins {
+        "VCC"
+    } else {
+        match chip {
+            Chip::GAL16V8 | Chip::GAL20V8 if gal.get_mode() == Mode::Mode3 && i == 1 => "Clock",
+            Chip::GAL16V8 if gal.get_mode() == Mode::Mode3 && i == 11 => "/OE",
+            Chip::GAL20V8 if gal.get_mode() == Mode::Mode3 && i == 13 => "/OE",
+            Chip::GAL22V10 if i == 1 => "Clock/Input",
+            _ => "Input",
+        }
+    }
+}
+
+fn make_pin(gal: &GAL, pin_names: &[String], olmcs: &[OLMC]) -> String {
     let mut buf = String::new();
     buf.push_str("\n\n");
     buf.push_str(" Pin # | Name     | Pin Type\n");
     buf.push_str("-----------------------------\n");
 
-    for n in 1..num_of_pins + 1 {
-        buf.push_str(&format!("  {:>2}   | ", n));
-        buf.push_str(&pin_names[n - 1]);
-
-        make_spaces(&mut buf, 9 - pin_names[n - 1].len());
-
-        let mut flag = false;
-
-        if n == num_of_pins / 2 {
-            buf.push_str("| GND\n");
-            flag = true;
-        }
-
-        if n == num_of_pins {
-            buf.push_str("| VCC\n\n");
-            flag = true;
-        }
-
-        if gal_type == Chip::GAL16V8 || gal_type == Chip::GAL20V8 {
-            let mode = gal.get_mode();
-
-            if mode == Mode::Mode3 && n == 1 {
-                buf.push_str("| Clock\n");
-                flag = true;
-            }
-
-            if mode == Mode::Mode3 {
-                if gal_type == Chip::GAL16V8 && n == 11 {
-                    buf.push_str("| /OE\n");
-                    flag = true;
-                }
-
-                if gal_type == Chip::GAL20V8 && n == 13 {
-                    buf.push_str("| /OE\n");
-                    flag = true;
-                }
-            }
-        }
-
-        if gal_type == Chip::GAL22V10 && n == 1 {
-            buf.push_str("| Clock/Input\n");
-            flag = true;
-        }
-
-        if let Some(olmc) = gal_type.pin_to_olmc(n) {
-            let olmc = &olmcs[olmc];
-            if olmc.output.is_some() {
-                buf.push_str("| Output\n");
-            } else if !olmc.feedback {
-                buf.push_str("| NC\n");
-            } else {
-                buf.push_str("| Input\n");
-            }
-        } else {
-            if !flag {
-                buf.push_str("| Input\n");
-            }
-        }
+    for (name, i) in pin_names.iter().zip(1..) {
+        buf.push_str(&format!("  {:>2}   | {:<8} | {}\n", i, name, pin_type(gal, olmcs, i)));
     }
+    buf.push_str("\n");
 
     return buf;
 }
@@ -170,31 +121,29 @@ fn make_fuse(
 ) -> String {
     let mut buf = String::new();
 
-    let gal_type = gal.chip;
-    let num_olmcs = gal_type.num_olmcs();
-    let row_len = gal_type.num_cols();
+    let chip = gal.chip;
+    let num_olmcs = chip.num_olmcs();
+    let row_len = chip.num_cols();
 
-    let mut pin = gal_type.last_olmc();
+    let mut pin = chip.last_olmc();
     let mut row = 0;
 
     for olmc in 0..num_olmcs {
-        if gal_type == Chip::GAL22V10 && olmc == 0 {
+        if chip == Chip::GAL22V10 && olmc == 0 {
             // AR when 22V10
             buf.push_str("\n\nAR");
             make_row(&mut buf, row_len, row, &gal.fuses);
             row += 1;
         }
 
-        let num_rows = gal_type.num_rows_for_olmc(olmc);
+        let num_rows = chip.num_rows_for_olmc(olmc);
 
         // Print pin
         buf.push_str(&format!("\n\nPin {:>2} = ", pin));
 
-        buf.push_str(&format!("{}", pin_names[pin - 1]));
+        buf.push_str(&format!("{:<13}", pin_names[pin - 1]));
 
-        make_spaces(&mut buf, 13 - pin_names[pin - 1].len());
-
-        match gal_type {
+        match chip {
             Chip::GAL16V8 => {
                 buf.push_str(&format!("XOR = {:>1}   AC1 = {:>1}", b(gal.xor[19 - pin]), b(gal.ac1[19 - pin])));
             }
@@ -215,7 +164,7 @@ fn make_fuse(
             row += 1;
         }
 
-        if gal_type == Chip::GAL22V10 && olmc == 9 {
+        if chip == Chip::GAL22V10 && olmc == 9 {
             // SP when 22V10
             buf.push_str("\n\nSP");
             make_row(&mut buf, row_len, row, &gal.fuses);
@@ -246,7 +195,6 @@ pub fn write_files(
     gal: &GAL,
 ) -> Result<(), Error> {
     let base = PathBuf::from(file_name);
-    let gal_type = gal.chip;
 
     write_file(&base, "jed", &::jedec_writer::make_jedec(config, gal))?;
 
@@ -259,7 +207,7 @@ pub fn write_files(
     }
 
     if config.gen_chip {
-        write_file(&base, "chp", &make_chip(gal_type, pin_names))?;
+        write_file(&base, "chp", &make_chip(gal.chip, pin_names))?;
     }
 
     Ok(())
