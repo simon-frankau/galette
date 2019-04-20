244e2f94ccae0efb16c61c03698746c355b8699e
Simon Frankau <sgf@arbitrary.name>
2019-04-20 17:27:35 +0100

    Refactor gal_builder.

--- src/gal_builder.rs
+++ src/gal_builder.rs
@@ -11,6 +11,8 @@ use gal::GAL;
 use gal::Mode;
 use olmc;
 
+// TODO: This takes a mutating view of the world. It could be
+// constructed rather more functionally, field-by-field.
 
 pub fn build(blueprint: &mut Blueprint) -> Result<GAL, Error> {
     let mut gal = GAL::new(blueprint.chip);
@@ -25,9 +27,13 @@ pub fn build(blueprint: &mut Blueprint) -> Result<GAL, Error> {
         Chip::GAL20RA10 => build_gal20ra10(&mut gal, blueprint)?,
     }
 
+    // All chips handle XOR on the outputs in the same way.
+    build_xors(&mut gal, blueprint);
+
     Ok(gal)
 }
 
+// Write out the signature.
 fn set_sig(blueprint: &Blueprint, gal: &mut GAL) {
     // Signature has space for 8 bytes.
     for i in 0..usize::min(blueprint.sig.len(), 8) {
@@ -40,7 +46,7 @@ fn set_sig(blueprint: &Blueprint, gal: &mut GAL) {
 
 // Adjust the bounds for the main term of there's a tristate enable
 // term in the first row.
-pub fn tristate_adjust(gal: &GAL, output: &Option<(PinMode, gal::Term)>, bounds: &Bounds) -> Bounds {
+fn tristate_adjust(gal: &GAL, output: &Option<(PinMode, gal::Term)>, bounds: &Bounds) -> Bounds {
     match gal.chip {
         Chip::GAL16V8 | Chip::GAL20V8 => {
             let reg_out = if let Some((PinMode::Registered, _)) = output { true } else { false };
@@ -71,24 +77,50 @@ fn check_gal20ra10(blueprint: &mut Blueprint) -> Result<(), Error> {
     Ok(())
 }
 
+// Set the XOR bits for inverting outputs, if necessary.
+fn build_xors(gal: &mut GAL, blueprint: &mut Blueprint) {
+    let num_olmcs = blueprint.olmcs.len();
+    for (olmc, i) in blueprint.olmcs.iter().zip(0..) {
+        if olmc.output.is_some() && olmc.active == Active::High {
+            gal.xor[num_olmcs - 1 - i] = true;
+        }
+    }
+}
+
+// Build the tristate control bits - set for inputs and tristated outputs.
+fn build_tristate_flags(flags: &mut [bool], blueprint: &mut Blueprint) {
+    let num_olmcs = blueprint.olmcs.len();
+    for (olmc, i) in blueprint.olmcs.iter().zip(0..) {
+        let is_tristate = match olmc.output {
+            None => olmc.feedback,
+            Some((PinMode::Tristate, _)) => true,
+            _ => false,
+        };
+
+        if is_tristate {
+            flags[num_olmcs - 1 - i] = true;
+        }
+    }
+}
+
 ////////////////////////////////////////////////////////////////////////
 // Chip-specific GAL-building algorithms.
 //
 
 // Core of the GALxxV8 and GAL22V10 builders.
 fn build_galxvx(gal: &mut GAL, blueprint: &mut Blueprint) -> Result<(), Error> {
-    for i in 0..blueprint.olmcs.len() {
+    for (olmc, i) in blueprint.olmcs.iter().zip(0..) {
         let bounds = gal.chip.get_bounds(i);
 
-        match &blueprint.olmcs[i].output {
+        match &olmc.output {
             Some((_, term)) => {
-                let bounds = tristate_adjust(gal, &blueprint.olmcs[i].output, &bounds);
+                let bounds = tristate_adjust(gal, &olmc.output, &bounds);
                 gal.add_term(&term, &bounds)?;
             }
             None => gal.add_term(&gal::false_term(0), &bounds)?,
         }
 
-        if let Some(term) = &blueprint.olmcs[i].tri_con {
+        if let Some(term) = &olmc.tri_con {
             gal.add_term(&term, &Bounds { row_offset: 0, max_row: 1, ..bounds })?;
         }
     }
@@ -97,34 +129,25 @@ fn build_galxvx(gal: &mut GAL, blueprint: &mut Blueprint) -> Result<(), Error> {
 }
 
 fn build_galxv8(gal: &mut GAL, blueprint: &mut Blueprint) -> Result<(), Error> {
+    check_gal20ra10(blueprint)?;
+
     // SYN and AC0 already defined.
 
+    build_galxvx(gal, blueprint)?;
+
+    build_tristate_flags(&mut gal.ac1, blueprint);
+
+    // We don't do anything with the PT bits.
     for n in 0..64 {
         gal.pt[n] = true;
     }
 
-    for n in 0..8 {
-        if match blueprint.olmcs[n].output {
-            None => blueprint.olmcs[n].feedback,
-            Some((PinMode::Tristate, _)) => true,
-            _ => false,
-        } {
-            gal.ac1[7 - n] = true;
-        }
-    }
-
-    for n in 0..8 {
-        if blueprint.olmcs[n].output.is_some() && blueprint.olmcs[n].active == Active::High {
-            gal.xor[7 - n] = true;
-        }
-    }
-
-    check_gal20ra10(blueprint)?;
-    build_galxvx(gal, blueprint)?;
     Ok(())
 }
 
 fn build_gal22v10(gal: &mut GAL, blueprint: &mut Blueprint) -> Result<(), Error> {
+    check_gal20ra10(blueprint)?;
+
     for n in 0..10 {
         // Make combinatorial terms into tristates.
         if let Some((ref mut pin_mode, _)) = blueprint.olmcs[n].output {
@@ -132,21 +155,12 @@ fn build_gal22v10(gal: &mut GAL, blueprint: &mut Blueprint) -> Result<(), Error>
                 *pin_mode = PinMode::Tristate;
             }
         }
-
-        if blueprint.olmcs[n].output.is_some() && blueprint.olmcs[n].active == Active::High {
-            gal.xor[9 - n] = true;
-        }
-
-        if match blueprint.olmcs[n].output {
-            None => blueprint.olmcs[n].feedback,
-            Some((PinMode::Tristate, _)) => true,
-            _ => false,
-        } {
-            gal.s1[9 - n] = true;
-        }
     }
 
-    check_gal20ra10(blueprint)?;
+    // TODO: Needs to be called before all the set_ands. Would be nice
+    // to make independent.
+    build_tristate_flags(&mut gal.s1, blueprint);
+
     build_galxvx(gal, blueprint)?;
 
     // AR
@@ -192,10 +206,6 @@ fn build_gal20ra10(gal: &mut GAL, blueprint: &Blueprint) -> Result<(), Error> {
 
             let clock_bounds = Bounds { row_offset: 1, max_row: 2, .. bounds };
             gal.add_term_opt(&olmc.clock, &clock_bounds)?;
-        }
-
-        if olmc.output.is_some() && olmc.active == Active::High {
-            gal.xor[9 - i] = true;
         }
     }
 
