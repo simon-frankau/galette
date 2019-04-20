fb07688fd573b4e4eb15d77cb0d62a1b38b0babc
Simon Frankau <sgf@arbitrary.name>
2019-04-20 21:14:01 +0100

    Attempt to structure the builders more uniformly

--- src/gal_builder.rs
+++ src/gal_builder.rs
@@ -11,22 +11,14 @@ use gal::Bounds;
 use gal::GAL;
 use gal::Mode;
 
-// TODO: This takes a mutating view of the world. It could be
-// constructed rather more functionally, field-by-field.
-
 pub fn build(blueprint: &Blueprint) -> Result<GAL, Error> {
     let mut gal = GAL::new(blueprint.chip);
-
-    set_sig(&blueprint, &mut gal);
 
     match gal.chip {
         Chip::GAL16V8 | Chip::GAL20V8 => build_galxv8(&mut gal, blueprint)?,
         Chip::GAL22V10 => build_gal22v10(&mut gal, blueprint)?,
         Chip::GAL20RA10 => build_gal20ra10(&mut gal, blueprint)?,
     }
-
-    // All chips handle XOR on the outputs in the same way.
-    build_xors(&mut gal, blueprint);
 
     Ok(gal)
 }
@@ -39,6 +31,73 @@ fn set_sig(blueprint: &Blueprint, gal: &mut GAL) {
         for j in 0..8 {
             gal.sig[i * 8 + j] = (c << j) & 0x80 != 0;
         }
+    }
+}
+
+// Set the main expression and tristate.
+fn set_core_eqns(gal: &mut GAL, blueprint: &Blueprint) -> Result<(), Error> {
+    for (olmc, i) in blueprint.olmcs.iter().zip(0..) {
+        let bounds = gal.chip.get_bounds(i);
+
+        match &olmc.output {
+            Some((_, term)) => {
+                let bounds = tristate_adjust(gal, &olmc.output, &bounds);
+                gal.add_term(&term, &bounds)?;
+            }
+            None => gal.add_term(&gal::false_term(0), &bounds)?,
+        }
+
+        if let Some(term) = &olmc.tri_con {
+            gal.add_term(&term, &Bounds { row_offset: 0, max_row: 1, ..bounds })?;
+        }
+    }
+
+    Ok(())
+}
+
+// Set ARST, APRST and CLK, only used by GAL20RA10
+fn set_aux_eqns(gal: &mut GAL, blueprint: &Blueprint) -> Result<(), Error> {
+    for (olmc, i) in blueprint.olmcs.iter().zip(0..) {
+        let bounds = gal.chip.get_bounds(i);
+
+        if olmc.output.is_some() {
+            if let Some((PinMode::Registered, ref term)) = olmc.output {
+                let arst_bounds = Bounds { row_offset: 2, max_row: 3, .. bounds };
+                gal.add_term_opt(&olmc.arst, &arst_bounds)?;
+
+                let aprst_bounds = Bounds { row_offset: 3, max_row: 4, .. bounds };
+                gal.add_term_opt(&olmc.aprst, &aprst_bounds)?;
+
+                if olmc.clock.is_none() {
+                    return at_line(term.line_num, Err(ErrorCode::NoCLK));
+                }
+            }
+
+            let clock_bounds = Bounds { row_offset: 1, max_row: 2, .. bounds };
+            gal.add_term_opt(&olmc.clock, &clock_bounds)?;
+        }
+    }
+
+    Ok(())
+}
+
+// Set the AR and SP equations, unique to the GAL22V10
+fn set_arsp_eqns(gal: &mut GAL, blueprint: &Blueprint) -> Result<(), Error> {
+    // AR
+    let ar_bounds = Bounds { start_row: 0, max_row: 1, row_offset: 0 };
+    gal.add_term_opt(&blueprint.ar, &ar_bounds)?;
+
+    // SP
+    let sp_bounds = Bounds { start_row: 131, max_row: 1, row_offset: 0 };
+    gal.add_term_opt(&blueprint.sp, &sp_bounds)?;
+
+    Ok(())
+}
+
+// We don't do anything with the PT bits in the GALxxV8s.
+fn set_pts(gal: &mut GAL) {
+    for bit in gal.pt.iter_mut() {
+        *bit = true;
     }
 }
 
@@ -55,12 +114,12 @@ fn tristate_adjust(gal: &GAL, output: &Option<(PinMode, gal::Term)>, bounds: &Bo
             }
         }
         Chip::GAL22V10 => Bounds { row_offset: 1, ..*bounds },
-        Chip::GAL20RA10 => panic!("Nope!"),
+        Chip::GAL20RA10 => Bounds { row_offset: 4, .. *bounds },
     }
 }
 
 // Check that you're not trying to use 20ra10-specific features
-fn check_gal20ra10(blueprint: &Blueprint) -> Result<(), Error> {
+fn check_not_gal20ra10(blueprint: &Blueprint) -> Result<(), Error> {
     for olmc in blueprint.olmcs.iter() {
         if let Some(term) = &olmc.clock {
             return at_line(term.line_num, Err(ErrorCode::DisallowedCLK));
@@ -76,7 +135,7 @@ fn check_gal20ra10(blueprint: &Blueprint) -> Result<(), Error> {
 }
 
 // Set the XOR bits for inverting outputs, if necessary.
-fn build_xors(gal: &mut GAL, blueprint: &Blueprint) {
+fn set_xors(gal: &mut GAL, blueprint: &Blueprint) {
     let num_olmcs = blueprint.olmcs.len();
     for (olmc, i) in blueprint.olmcs.iter().zip(0..) {
         if olmc.output.is_some() && olmc.active == Active::High {
@@ -139,104 +198,54 @@ pub fn get_mode_v8(olmcs: &[OLMC]) -> Mode {
 // Chip-specific GAL-building algorithms.
 //
 
-// Core of the GALxxV8 and GAL22V10 builders.
-fn build_galxvx(gal: &mut GAL, blueprint: &Blueprint) -> Result<(), Error> {
-    for (olmc, i) in blueprint.olmcs.iter().zip(0..) {
-        let bounds = gal.chip.get_bounds(i);
-
-        match &olmc.output {
-            Some((_, term)) => {
-                let bounds = tristate_adjust(gal, &olmc.output, &bounds);
-                gal.add_term(&term, &bounds)?;
-            }
-            None => gal.add_term(&gal::false_term(0), &bounds)?,
-        }
-
-        if let Some(term) = &olmc.tri_con {
-            gal.add_term(&term, &Bounds { row_offset: 0, max_row: 1, ..bounds })?;
-        }
-    }
-
-    Ok(())
-}
+// TODO: Build order:
+// X sig
+// X ac0 and syn
+// X fuses
+// X xor
+// ac1/s1
+// X pt
 
 fn build_galxv8(gal: &mut GAL, blueprint: &Blueprint) -> Result<(), Error> {
-    check_gal20ra10(blueprint)?;
+    check_not_gal20ra10(blueprint)?;
+    set_sig(blueprint, gal);
 
     let mode = get_mode_v8(&blueprint.olmcs);
+    // Sets AC0 and SYN
     gal.set_mode(mode);
+
     // Are we implementing combinatorial expressions as tristate?
     // Put combinatorial is only available in Mode 1.
     let com_is_tri = mode != Mode::Mode1;
 
-    // SYN and AC0 already defined.
-
-    build_galxvx(gal, blueprint)?;
+    set_core_eqns(gal, blueprint)?;
 
     build_tristate_flags(&mut gal.ac1, blueprint, com_is_tri);
 
-    // We don't do anything with the PT bits.
-    for n in 0..64 {
-        gal.pt[n] = true;
-    }
+    set_xors(gal, blueprint);
+    set_pts(gal);
 
     Ok(())
 }
 
 fn build_gal22v10(gal: &mut GAL, blueprint: &Blueprint) -> Result<(), Error> {
-    check_gal20ra10(blueprint)?;
+    check_not_gal20ra10(blueprint)?;
+    set_sig(blueprint, gal);
 
     // TODO: Needs to be called before all the set_ands. Would be nice
     // to make independent.
     build_tristate_flags(&mut gal.s1, blueprint, true);
 
-    build_galxvx(gal, blueprint)?;
-
-    // AR
-    let ar_bounds = Bounds { start_row: 0, max_row: 1, row_offset: 0 };
-    gal.add_term_opt(&blueprint.ar, &ar_bounds)?;
-
-    // SP
-    let sp_bounds = Bounds { start_row: 131, max_row: 1, row_offset: 0 };
-    gal.add_term_opt(&blueprint.sp, &sp_bounds)?;
-
+    set_core_eqns(gal, blueprint)?;
+    set_arsp_eqns(gal, blueprint)?;
+    set_xors(gal, blueprint);
     Ok(())
 }
 
 fn build_gal20ra10(gal: &mut GAL, blueprint: &Blueprint) -> Result<(), Error> {
-    for (olmc, i) in blueprint.olmcs.iter().zip(0..) {
-        let bounds = gal.chip.get_bounds(i);
-
-        match &olmc.output {
-            // Tristate, combinatorial or registered, fuses go in the same place.
-            Some((_, term)) => {
-                gal.add_term(&term, &Bounds { row_offset: 4, .. bounds })?;
-            }
-            None => gal.add_term(&gal::false_term(0), &bounds)?,
-        }
-
-        // If not set, is true (and permanently enabled).
-        if let Some(term) = &olmc.tri_con {
-            gal.add_term(&term, &Bounds { row_offset: 0, max_row: 1, .. bounds })?;
-        }
-
-        if olmc.output.is_some() {
-            if let Some((PinMode::Registered, ref term)) = olmc.output {
-                let arst_bounds = Bounds { row_offset: 2, max_row: 3, .. bounds };
-                gal.add_term_opt(&olmc.arst, &arst_bounds)?;
-
-                let aprst_bounds = Bounds { row_offset: 3, max_row: 4, .. bounds };
-                gal.add_term_opt(&olmc.aprst, &aprst_bounds)?;
-
-                if olmc.clock.is_none() {
-                    return at_line(term.line_num, Err(ErrorCode::NoCLK));
-                }
-            }
-
-            let clock_bounds = Bounds { row_offset: 1, max_row: 2, .. bounds };
-            gal.add_term_opt(&olmc.clock, &clock_bounds)?;
-        }
-    }
-
+    set_sig(blueprint, gal);
+    set_core_eqns(gal, blueprint)?;
+    set_aux_eqns(gal, blueprint)?;
+    set_xors(gal, blueprint);
     Ok(())
 }
